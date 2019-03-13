f2639cf98df726eca55b8e206411d5d8790c34e7
Edvin Boul <kittenparry@users.noreply.github.com>
2019-03-13 13:13:36 +0300

    Filter and regex append in one file

--- clear_duplicates.py
+++ clear_duplicates.py
@@ -1,15 +1,26 @@
 
-# Creates a list with duplicates removed
+# Creates a list with duplicates removed,
+# names starting with underscore or dash or is 'unorganised' filtered
+# and regex appended
 
 cleared_list = filter(None, open('list.txt').read().split('\n'))
 cleared_list = list(dict.fromkeys(cleared_list))
+temp = []
 
-# TODO: remove if starts with underscore or is 'unorganised'
-# TODO: also put append_regex.py here so there won't be 2 different files
+for el in cleared_list:
+	# don't add names that start with underscore or dash or is 'unorganised'
+	if el.startswith('_') or el.startswith('-') or el == 'unorganised':
+		# for whatever reason this didn't catch a few, so we're doing it the other way
+		# cleared_list.remove(el)
+		pass
+	else:
+		# replace _ with regex whitespace or dot
+		el = el.replace('_', '[\. ]')
+		temp.append(el)
 
 try:
 	f = open('filtered_list.txt', 'w')
-	for l in sorted(cleared_list):
+	for l in sorted(temp):
 		f.write("%s\n" % l)
 except IOError as e:
 	print(e)
